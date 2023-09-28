54a26ac4b64fc4185e934ea20eb3b5826a182a6c
AlejoProjects <alejodelavinci@gmail.com>
2023-09-28 15:54:37 -0500

    css is beginning to be implemented

--- src/Pages/userInformation.jsx
+++ src/Pages/userInformation.jsx
@@ -10,12 +10,13 @@ const InformacionUsuario = ({info})=> {
  */
     return(
         <>
-            <div>
-                    <p>{info[0]} {info[1]}</p>
-                    <p>{info[2]}</p>
-                    <p>{info[3]} </p>
-                    <p>{info[4]}</p>
-                    <p>{info[5]} </p>
+            <div id='informacion_completa' >
+                    <p id="item1">{info[0]} {info[1]}</p>
+                    <p id="item2">{info[2]}</p>
+                    <p id="item3">{info[3]} </p>
+                    <p id="item5">{info[5]} </p>
+                    <p id="item4">{info[4]}</p>
+                    
                  
             </div>
         </>
