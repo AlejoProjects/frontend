1c9af61e244e3e21e96403f2cfd34ceba6a92e21
AlejoProjects <alejodelavinci@gmail.com>
2023-10-03 19:07:28 -0500

    mecanismo para poder editar info implementado

--- src/Pages/configuration.jsx
+++ src/Pages/configuration.jsx
@@ -1,95 +1,90 @@
-import axios from 'axios';
-import { useState , useEffect } from "react";
+import axios from "axios";
+import { useState, useEffect } from "react";
 import NavMenu from "./nav";
 import persona from "../assets/persona.png";
-import ConfigurationElement from "./configurationElement";
+import ConfigurationElement from "../Pages/configurationElement";
 import "../css/configuration.css";
-const Configuration = () => {
-  const  id_usuario = 1;
-  const [data,setData] = useState([]);
-/**Esta función es temporal, se borrara cuando sea implementada la validación del login*/
-let contenido = ['','','','','','',''];
-let fetc = '';
-const identificador = (identificador) => {
-  switch(identificador){
-    case 'usuario':
-      fetc = 'http://localhost:3000/api/v1/Personas/';
-    break;
-    case 'empresa':
-      fetc = 'http://localhost:3000/api/v1/empresa/';
-    break;
-  };
-  
-  return(fetc);
-};
-const identificadorContainer = (identificador) => {
-  switch(identificador){
-    case 'usuario':
-      contenido[0]=data.nombre_persona;
-      contenido[1]=data.apellido_persona;
-      contenido[2]=data.locacion;
-      contenido[3]=data.email;
-      contenido[4]=data.precio_servicio;
-      contenido[5]=data.estatus;
-      contenido[6]=data.password;
-      contenido.push(data.perfil);
-      contenido.push(data.calificacion);
 
-    break;
-    case 'empresa':
-      contenido[0]=data.nombre_empresa;
-      contenido[1]=data.nit;
-      contenido[2]=data.direccion;
-      contenido[3]=data.email;
-      contenido[4]=data.telefono_empresa;
-      contenido[5]=data.status;
-      contenido[6]=data.password;
-    break;
+const Configuration = () => {
+  const [data, setData] = useState([]);
+  let infoContainer = ["", "", "", "", "", ""];
+  const iden = "usuario";
+  const clasificador = (iden) => {
+    switch (iden) {
+      case "usuario":
+        infoContainer = [
+          data.id_persona,
+          data.nombre_persona,
+          data.email,
+          data.locacion,
+          data.password,
+          data.status,
+        ];
+        infoContainer.push(data.apellido_persona);
+        infoContainer.push(data.precio_servicio);
+        infoContainer.push(data.perfil);
+        infoContainer.push(data.calificacion);
+        break;
+      case "empresa":
+        infoContainer = [
+          data.id,
+          data.nombre_empresa,
+          data.email,
+          data.direccion,
+          data.password,
+          data.estatus,
+        ];
+        infoContainer.push(data.nit);
+        infoContainer.push(data.telefono_empresa);
+    }
   };
-  
-  return(contenido);
-};
-useEffect(() => {
-  fetchData();
-});
+  /**Esta función es temporal, se borrara cuando sea implementada la validación del login*/
+  const id_usuario = 1;
+  useEffect(() => {
+    fetchData();
+  });
   const fetchData = async () => {
-    const iden = 'usuario';
-    const info = identificador(iden);
-    
-    fetch(info+`?&id=${id_usuario}`)
+    fetch(`http://localhost:3000/api/v1/Personas/` + `?&id=${id_usuario}`)
       .then((response) => response.json())
       .then((jsonData) => {
         setData(jsonData.message[id_usuario]);
-        identificador('persona');
+      
+       
       })
       .catch((error) => console.log("Ocurrió un error en la consulta"));
-  }
-  identificadorContainer('usuario');
-/**Aqui finaliza la parte a ser removida del codigo */
-  
+  };
+  /**Aqui finaliza la parte a ser removida del codigo */
+  /**la idea es realizar el recorrido de contenido[i] con un map (recordarlo) */
+  clasificador(iden);
+  const InformationMapping =infoContainer.map((item,index) => (
+   
+     <ConfigurationElement key={index} item={item} index={index} ident={iden}/>
+  ));
+
+
   return (
     <>
       <NavMenu></NavMenu>
       <div id="title">
-      <img src={persona} className="circularPicture" id="profilePicture"></img>
-      <h2>nombre apellido</h2>
+        <img
+          src={persona}
+          className="circularPicture"
+          id="profilePicture"
+        ></img>
+        <h2>nombre apellido</h2>
       </div>
+      
       <div className="elementsContainer">
         <h3>Información personal</h3>
-        <ConfigurationElement valor={contenido[0]}></ConfigurationElement>
-        <ConfigurationElement valor={contenido[1]}></ConfigurationElement>
-        <ConfigurationElement valor={contenido[2]}></ConfigurationElement>
-        <ConfigurationElement valor={contenido[3]}></ConfigurationElement>
+      
+        {InformationMapping}
       </div>
       <div className="elementsContainer">
         <h3>Información sensible</h3>
-        <ConfigurationElement valor={contenido[4]}></ConfigurationElement>
-        <ConfigurationElement valor={contenido[8]}></ConfigurationElement>
-        <ConfigurationElement valor={contenido[6]}></ConfigurationElement>
 
       </div>
       <div></div>
-  </>
-  )
+    </>
+  );
 };
 export default Configuration;
--- src/Pages/configurationElement.jsx
+++ src/Pages/configurationElement.jsx
@@ -1,12 +1,43 @@
+import axios from 'axios';
+import { useState , useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPencil} from "@fortawesome/free-solid-svg-icons";
-const ConfigurationElement = ({valor}) => {
-  
+const ConfigurationElement = (props) => {
+  const divIden ="updateInfo"+ props.index;
+  const [texto,setTexto] = useState('');
+  const visibilidadTemporal = (valor)=> {
+    const divTemporal = document.getElementById(divIden);
+    switch(valor){
+      case 1:divTemporal.style.display = 'flex';
+      break;
+      case 0:divTemporal.style.display = 'none'
+    }
+  };
+
     return (
       <>
       <div className="configComponent">
-        <p id="textComponent">{valor}</p>
-        <button className="circularPicture buttonContainer"> <FontAwesomeIcon icon={faPencil} className="circularPicture" id="icon"/></button>
+        <p className='textComponent'>{props.item} </p>
+        <button className="circularPicture buttonContainer" onClick={() => {
+          const divTemporal = document.getElementById(divIden);
+          divTemporal.style.display = 'flex';
+        }}> <FontAwesomeIcon icon={faPencil} className="circularPicture" id="icon"/></button>
+        <div className="edit" id={divIden}>
+        <input type="text" value={texto} onChange={(event) => {setTexto(event.target.value)}}></input>
+        <button onClick={async () => {
+            console.log("holiwi");
+            console.log(props.ident);
+            visibilidadTemporal(0);
+            /**try {
+              const response = await axios.put(`/api/v1/${valor[0]}/${valor[5]}/`, texto);// /api/v1/persona o empresa/id
+              // Handle success, update UI if necessary
+            } 
+            catch (error) {
+              // Handle errors
+            }*/
+        }}>confirmar</button>
+        <button>cancelar</button> 
+        </div>
     </div>
     </>
     )
