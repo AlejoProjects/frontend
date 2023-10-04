071f1520496755279d760d20fa6420c0377a5302
Santiago Gonzalez <santiagogonzaric1015@gmail.com>
2023-10-04 00:12:13 -0500

    Feat: create Login module

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,22 +1,65 @@
-import { useState } from "react";
+import React,{ useState } from "react";
+import axios  from "axios";
 import "../css/stylesheet.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFire } from "@fortawesome/free-solid-svg-icons";
 import NavMenu from "./nav";
+
+
+
 const Home = () => {
+  const [datos, setDatos] = useState({email: "",type: "",password: ""});
+
+
+const handleInputChage = (e) => {
+  let {name, value} = e.target;
+  let newDatos = {...datos, [name]: value};
+
+  setDatos(newDatos);
+};
+
+const handleSubmit = async (e)=> {
+  e.preventDefault();
+  if (!e.target.checkValidity()) {
+    console.log("no enviar");
+  }else{
+    let res = await axios.post("http://localhost:3000/api/v1/Login/", datos);
+    console.log(res.data);
+  }
+}
   return (
     <>
-      <NavMenu></NavMenu>
-      <h1>Tinder Para Habilidades</h1>
-      <div id="login_div">
-        <div className="row_elements">
-          <p>User</p>
-          <input id="user_input"></input>
-        </div>
-        <div className="row_elements">
-          <p>Password</p>
-          <input></input>
-        </div>
-        <button>Login</button>
-      </div>
+       <div className="logo-box">
+       <FontAwesomeIcon
+        icon={faFire}
+        style={{
+          WebkitMask: "radial-gradient(circle at 50% 10.64%, #d7c35d 0, #deba54 8.33%, #e4b04d 16.67%, #e9a546 25%, #ed9841 33.33%, #f0893e 41.67%, #f2793c 50%, #f3673d 58.33%, #f35542 66.67%, #f34249 75%, #f22b52 83.33%, #f0065d 91.67%, #ec0069 100%)",
+          mask: "radial-gradient(circle at 50% 10.64%, transparent 0, transparent 8.33%, transparent 16.67%, transparent 25%, transparent 33.33%, transparent 41.67%, transparent 50%, transparent 58.33%, transparent 66.67%, transparent 75%, transparent 83.33%, transparent 91.67%, transparent 100%)",
+          color: "red", // Cambia el color del ícono aquí
+        }}
+      />
+
+      <h2>Iniciar sesión en Tinder de habilidades</h2>
+      
+      <hr></hr>
+      <form onSubmit={handleSubmit} noValidate={true} autoComplete="off">
+
+        <select name="type" id="typeUser" required value={datos.type} onChange={handleInputChage}>
+          <option value="" disabled >Seleccione una opción</option>
+          <option value="empresa">Empresas</option>
+          <option value="user">Personas</option>
+        </select>
+
+        <input id="email" type="text" onChange={handleInputChage} value={datos.email} placeholder="Correo electrónico" name = "email" required/>
+
+        <input id="password" type="password" onChange={handleInputChage} value={datos.password} placeholder="Contraseña" name= "password" required />
+        <button>Ingresar</button>
+      </form>
+      <button>Forget Password</button>
+      <p>
+        Don't Have an account<a>Sign up</a>
+      </p>
+    </div>
     </>
   );
 };
