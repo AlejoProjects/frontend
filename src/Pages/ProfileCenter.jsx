370924ee458f7d036979af0cf9701e29de8528bf
AlejoProjects <alejodelavinci@gmail.com>
2023-10-11 09:18:34 -0500

    se añaden las funciones de comprobación de servicios

--- src/Pages/ProfileCenter.jsx
+++ src/Pages/ProfileCenter.jsx
@@ -1,8 +1,8 @@
-import { useState , useEffect , useContext} from "react";
+import { useState , useEffect} from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHeart,faX,faBolt,faRefresh,faStar,} from "@fortawesome/free-solid-svg-icons";
 import "../css/ProfileCenter.css";
-import persona from "../assets/persona.png";
+import persona from "../assets/persona.jpg";
 import NavMenu from "./nav";
 import InformacionUsuario from "./userInformation";
 import axios from "axios";
@@ -13,7 +13,7 @@ const ProfileCenter = () => {
   const [counter, setCounter] = useState(0);
   const [data,setData] = useState([]);
   const [size, setSize] = useState(0);
-
+  const [servicios,setServicios] = useState([]);
   const empresaId =  1;
   /**Este efecto llama a cada persona mostrada en la carta */
   useEffect(() => {
@@ -47,22 +47,51 @@ const ProfileCenter = () => {
       });
       const jsonData = response.data.message[counter];
       setData(jsonData);
-      setId(jsonData.id_persona);
     } catch (error) {
       console.log('Ocurrió un error en la consulta ' + error.message);
     }
+  };
+  
+  const fetchServices = async () => {
+    /**Se hace el llamado de los servicios para consultar si ya existen o no. */
+    try {
+      const response = await axios.get(`http://localhost:3000/api/v1/Servicios`, {
+      /**   headers: {
+          Authorization: `Bearer ${user.token}`,
+        },*/
+      });
+      const jsonData = response.data.message[counter];
+      setServicios(jsonData);
+    } catch (error) {
+      console.log('Ocurrió un error en la consulta ' + error.message);
+    }
+  };
+  /**Esta función valida si ya existe un servicio creado dentro de la base de datos. Si esta creado entonces ignora la muestra de los datos de esa persona. */
+  const validadorServicios = async () => {
+    const sizeServicios = servicios.length;
+    for(let i = 0;i < sizeServicios;i+= 1){
+      if(parseInt(servicios[i].fk_empresa_id)==empresaId){
+          if(parseInt(servicios[i].fk_persona_id)==data.id_persona){
+            setCounter(counter + 1);
+        }
+      }
+    };
+
   };
    
   
   return (
     <>
+ 
+      <div id="contenedorGeneral">
       <NavMenu></NavMenu>
-      <div>
       <InformacionUsuario info={[data.nombre_persona,data.apellido_persona,data.locacion,data.precio_servicio,data.perfil,data.calificacion]}></InformacionUsuario>
        <h3>
           {data.nombre_persona}&nbsp;{data.apellido_persona}
         </h3>
-        <img src={persona}></img>
+        <button className="elements" onClick={()=>{console.log('hallo')}}>
+        <img src={persona} id="imagenProfile"></img>
+       </button>
         <div className="row_elements profile_icons">
           <button
             className="elements"
@@ -95,7 +124,6 @@ const ProfileCenter = () => {
                 nombre_servicio:"match",
 	              descripción:"se ha generado la solicitud del servicio"
           };  
-            logConsole(valores);
               //creación de servicio
               try {
                 const response = await axios.post(`http://localhost:3000/api/v1/Servicios`, valores);
@@ -116,9 +144,6 @@ const ProfileCenter = () => {
             }}
           >
             <FontAwesomeIcon icon={faHeart} className="icon" id="heart" />
-          </button>
-          <button className="elements">
-            <FontAwesomeIcon icon={faBolt} className="icon" id="ray" />
           </button>
         </div>
       </div>
