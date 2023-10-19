902ff052ebf63453b37b15c21c7a9018ae044294
AlejoProjects <alejodelavinci@gmail.com>
2023-10-19 11:34:14 -0500

    image update

--- src/Pages/Entidad.jsx
+++ src/Pages/Entidad.jsx
@@ -1,4 +1,3 @@
-import persona from "/src/assets/persona.png";
 import "/src/css/Servicio.css";
 const Entidad = (props) => {
   return (
@@ -6,7 +5,7 @@ const Entidad = (props) => {
       <div className="box box_size">
       <h3>habilidades{props.nombre_empresa}</h3>
       <div className="text_container">
-      <img  src={persona} className=".circularPicture"/>
+      <img  src="/opt/build/repo/src/assets/1.png" className=".circularPicture"/>
       <p className="message_snipet">proceso iniciado</p>
       </div>
      </div>
