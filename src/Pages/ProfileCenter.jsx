888dca6d4d70ea9352c5fd91d3a7d7341285fb24
AlejoProjects <alejodelavinci@gmail.com>
2023-10-19 12:01:28 -0500

    ,

--- src/Pages/ProfileCenter.jsx
+++ src/Pages/ProfileCenter.jsx
@@ -7,7 +7,7 @@ import { useAuth } from "./AuthContext";
 import axios from "axios";
 import Modal from 'react-bootstrap/Modal';
 import NavMenu from "./NavMenu";
-import {i1,i2,i3,i4,i5,i6,i7,i8,i9,i10} from "/opt/build/repo/src/assets/images";
+import {i1,i2,i3,i4,i5,i6,i7,i8,i9,i10} from "../assets/images";
 
 
 
