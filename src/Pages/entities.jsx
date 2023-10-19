46f0d883ebe881e6294b9deeb4ed8d2192a60d77
AlejoProjects <alejodelavinci@gmail.com>
2023-10-19 08:23:52 -0500

    entities errors fixed

--- src/Pages/entities.jsx
+++ src/Pages/entities.jsx
@@ -1,5 +1,5 @@
-import "/opt/build/repo/src/assets/persona.png"
-import persona from "/opt/build/repo/src/css/services.css";
+import persona from "/opt/build/repo/src/assets/persona.png";
+import "/opt/build/repo/src/css/services.css";
 const Entities = (props) => {
   return (
     <>
