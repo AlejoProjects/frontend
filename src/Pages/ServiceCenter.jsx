93c02430c8886d18b80bca0c4f11ae6ad1d376a3
AlejoProjects <alejodelavinci@gmail.com>
2023-10-20 01:45:06 -0500

    se añaden los imports necesarios

--- src/Pages/ServiceCenter.jsx
+++ src/Pages/ServiceCenter.jsx
@@ -1,13 +1,17 @@
 import NavMenu from "./NavMenu";
 import Entities from "./Entidad";
 import "/src/css/Servicio.css";
+import axios from "axios";
 import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { useAuth } from "./AuthContext";
+
 const ServiceCenter = () => {
-  const url = 'https://tinderhabilidadesapiv1.fly.dev/api/v1/';
+  const url = "https://tinderhabilidadesapiv1.fly.dev/api/v1/";
   const { user } = useAuth();
   const [servicios, setServicios] = useState([]);
   const tipo = user.type;
- useEffect(() => {
+  useEffect(() => {
     fetchServices();
   }, []);
 
@@ -18,7 +22,6 @@ const ServiceCenter = () => {
         .then((response) => {
           const jsonData = response.data.message;
           setServicios(jsonData);
-
         })
         .catch((error) => {
           console.error("Error fetching data:", error);
@@ -30,31 +33,29 @@ const ServiceCenter = () => {
   const createEntities = (usuario) => {
     let informacion = [];
     switch (usuario) {
-      case 'user':
+      case "user":
         for (let i = 0; i < servicios.length; i++) {
           if (servicios[i].id_persona == user.id) {
             informacion.push(servicios[i]);
           }
         }
         break;
-      case 'empresa':
+      case "empresa":
         for (let i = 0; i < servicios.length; i++) {
           if (servicios[i].id == user.id) {
             informacion.push(servicios[i]);
           }
         }
-    
+
         break;
     }
-    return(informacion);
-    
+    return informacion;
   };
   const mostrarServicios = () => {
     const usuariosPorMostrar = createEntities(tipo);
-  
-    return usuariosPorMostrar.map((usuario, index) => (
 
-      <Entities  key={index} datos={usuario} cual={tipo} />
+    return usuariosPorMostrar.map((usuario, index) => (
+      <Entities key={index} datos={usuario} cual={tipo} />
     ));
   };
 
@@ -66,7 +67,6 @@ const ServiceCenter = () => {
       </div>
     </>
   );
-
 };
 
 export default ServiceCenter;
