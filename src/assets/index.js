80d72ca84ffef24e8f21051f78943ca58a75166e
AlejoProjects <alejodelavinci@gmail.com>
2023-10-20 01:24:14 -0500

    se añade el contenido del index

---
+++ src/assets/index.js
@@ -0,0 +1,21 @@
+import i1 from "./1.png";
+import i2 from "./2.png";
+import i3 from "./3.png";
+import i4 from "./4.png";
+import i5 from "./5.png";
+import i6 from "./6.png";
+import i7 from "./7.png";
+import i8 from "./8.png";
+import i9 from "./9.png";
+import i10 from "./10.png";
+
+export { i1, 
+        i2,
+        i3, 
+        i4, 
+        i5, 
+        i6, 
+        i7, 
+        i8, 
+        i9, 
+        i10};
